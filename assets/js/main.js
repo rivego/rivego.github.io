a1326c6b9eeb79c463987425f2458098de3348b0
rivego <70266045+rivego@users.noreply.github.com>
2021-07-21 09:39:29 +0800

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,217 +1,87 @@
-/*
-	Helios by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+$(function () {
+  'use strict';
 
-(function($) {
+  /* -------- Scroll to top button ------- */
+  $(".top").click(function() {
+    $("html, body")
+      .stop()
+      .animate({ scrollTop: 0 }, "slow", "swing");
+  });
 
-	var	$window = $(window),
-		$body = $('body'),
-		settings = {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > $(window).height()) {
+      $(".top").addClass("is-active");
+    } else {
+      $(".top").removeClass("is-active");
+    }
+  });
 
-			// Carousels
-				carousels: {
-					speed: 4,
-					fadeIn: true,
-					fadeDelay: 250
-				},
+  // Cache variables for increased performance on devices with slow CPUs.
+  var flexContainer = $('div.flex-container')
+  var searchBox = $('.search-box')
+  var searchClose = $('.search-icon-close')
+  var searchInput = $('#search-input')
+  var waiting;
 
-		};
+  // Menu button
+  $('.menu-icon, .menu-icon-close').click(function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    if (flexContainer.hasClass('active')){
+      hideLayer();
+    } else {
+      flexContainer.addClass('active')
+      setTimeout(function () {
+        flexContainer.removeClass('transparent').addClass('opaque');
+      }, 10);
+    }
+  })
 
-	// Breakpoints.
-		breakpoints({
-			wide:      [ '1281px',  '1680px' ],
-			normal:    [ '961px',   '1280px' ],
-			narrow:    [ '841px',   '960px'  ],
-			narrower:  [ '737px',   '840px'  ],
-			mobile:    [ null,      '736px'  ]
-		});
+  // Click to close
+  flexContainer.click(function (e) {
+    if (flexContainer.hasClass('active') && e.target.tagName !== 'A') {
+      if (e.target.classList.contains('night')) {
+        clearTimeout(waiting);
+        waiting = setTimeout(function() {
+          hideLayer();
+        }, 1000);
+      } else {
+        hideLayer();
+      }
+    }
+  })
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  function hideLayer () {
+    flexContainer.removeClass('opaque')
+    flexContainer.addClass('transparent');
+    setTimeout(function(){
+      flexContainer.removeClass('active');
+    }, 600)
+  }
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			speed: 350,
-			noOpenerFade: true,
-			alignment: 'center'
-		});
+  // Press Escape key to close menu
+  $(window).keydown(function (e) {
+    if (e.key === 'Escape') {
+      if (flexContainer.hasClass('active')) {
+        hideLayer();
+      } else if (searchBox.hasClass('search-active')) {
+        searchBox.removeClass('search-active');
+      }
+    }
+  })
 
-	// Scrolly.
-		$('.scrolly').scrolly();
-
-	// Nav.
-
-		// Button.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
-	// Carousels.
-		$('.carousel').each(function() {
-
-			var	$t = $(this),
-				$forward = $('<span class="forward"></span>'),
-				$backward = $('<span class="backward"></span>'),
-				$reel = $t.children('.reel'),
-				$items = $reel.children('article');
-
-			var	pos = 0,
-				leftLimit,
-				rightLimit,
-				itemWidth,
-				reelWidth,
-				timerId;
-
-			// Items.
-				if (settings.carousels.fadeIn) {
-
-					$items.addClass('loading');
-
-					$t.scrollex({
-						mode: 'middle',
-						top: '-20vh',
-						bottom: '-20vh',
-						enter: function() {
-
-							var	timerId,
-								limit = $items.length - Math.ceil($window.width() / itemWidth);
-
-							timerId = window.setInterval(function() {
-								var x = $items.filter('.loading'), xf = x.first();
-
-								if (x.length <= limit) {
-
-									window.clearInterval(timerId);
-									$items.removeClass('loading');
-									return;
-
-								}
-
-								xf.removeClass('loading');
-
-							}, settings.carousels.fadeDelay);
-
-						}
-					});
-
-				}
-
-			// Main.
-				$t._update = function() {
-					pos = 0;
-					rightLimit = (-1 * reelWidth) + $window.width();
-					leftLimit = 0;
-					$t._updatePos();
-				};
-
-				$t._updatePos = function() { $reel.css('transform', 'translate(' + pos + 'px, 0)'); };
-
-			// Forward.
-				$forward
-					.appendTo($t)
-					.hide()
-					.mouseenter(function(e) {
-						timerId = window.setInterval(function() {
-							pos -= settings.carousels.speed;
-
-							if (pos <= rightLimit)
-							{
-								window.clearInterval(timerId);
-								pos = rightLimit;
-							}
-
-							$t._updatePos();
-						}, 10);
-					})
-					.mouseleave(function(e) {
-						window.clearInterval(timerId);
-					});
-
-			// Backward.
-				$backward
-					.appendTo($t)
-					.hide()
-					.mouseenter(function(e) {
-						timerId = window.setInterval(function() {
-							pos += settings.carousels.speed;
-
-							if (pos >= leftLimit) {
-
-								window.clearInterval(timerId);
-								pos = leftLimit;
-
-							}
-
-							$t._updatePos();
-						}, 10);
-					})
-					.mouseleave(function(e) {
-						window.clearInterval(timerId);
-					});
-
-			// Init.
-				$window.on('load', function() {
-
-					reelWidth = $reel[0].scrollWidth;
-
-					if (browser.mobile) {
-
-						$reel
-							.css('overflow-y', 'hidden')
-							.css('overflow-x', 'scroll')
-							.scrollLeft(0);
-						$forward.hide();
-						$backward.hide();
-
-					}
-					else {
-
-						$reel
-							.css('overflow', 'visible')
-							.scrollLeft(0);
-						$forward.show();
-						$backward.show();
-
-					}
-
-					$t._update();
-
-					$window.on('resize', function() {
-						reelWidth = $reel[0].scrollWidth;
-						$t._update();
-					}).trigger('resize');
-
-				});
-
-		});
-
-})(jQuery);
+  // Search button
+  $('.search-icon').click(function (e) {
+    e.preventDefault()
+    if($('.search-form.inline').length == 0){
+        searchBox.toggleClass('search-active')
+    }
+    searchInput.focus()
+    if (searchBox.hasClass('search-active')) {
+      searchClose.click(function (e) {
+    		e.preventDefault()
+    		searchBox.removeClass('search-active')
+    	})
+    }
+  })
+});
